da18ad81a01d90f67cbe6f7ad61383bc00fa8d0e
rChaoz <catalin_trand2@yahoo.com>
2023-04-30 15:51:00 +0300

    front: Improve LoadingOverlay
    Progress indicator no longer semi transparent

--- frontend/src/main/java/ro/bankar/app/ui/components/LoadingOverlay.kt
+++ frontend/src/main/java/ro/bankar/app/ui/components/LoadingOverlay.kt
@@ -2,7 +2,6 @@ package ro.bankar.app.ui.components
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Surface
@@ -17,27 +16,24 @@ fun LoadingOverlay(
     isLoading: Boolean,
     modifier: Modifier = Modifier,
     progress: Float? = null,
-    contentAlignment: Alignment = Alignment.TopStart,
-    propagateMinConstraints: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    Box(modifier, contentAlignment, propagateMinConstraints) {
+    Box(modifier, contentAlignment = Alignment.Center) {
         content()
-        if (isLoading) Surface(
-            modifier = modifier
-                .matchParentSize()
-                .alpha(.5f)
-        ) {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                if (progress == null) CircularProgressIndicator()
-                else {
-                    val animatedProgress by animateFloatAsState(
-                        targetValue = progress,
-                        label = "Progress Animation",
-                        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
-                    )
-                    CircularProgressIndicator(progress = animatedProgress)
-                }
+        if (isLoading) {
+            Surface(
+                modifier = modifier
+                    .matchParentSize()
+                    .alpha(.5f)
+            ) {}
+            if (progress == null) CircularProgressIndicator()
+            else {
+                val animatedProgress by animateFloatAsState(
+                    targetValue = progress,
+                    label = "Progress Animation",
+                    animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+                )
+                CircularProgressIndicator(progress = animatedProgress)
             }
         }
     }
