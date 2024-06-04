83ecb560ac2d46f66febff4d79c7d133a198e979
Matei-Paul Trandafir <matei_trand@yahoo.com>
2024-06-04 16:02:12 +0300

    Remove SSL support & move dev-mode to env variable

--- backend/src/main/kotlin/ro/bankar/Application.kt
+++ backend/src/main/kotlin/ro/bankar/Application.kt
@@ -1,21 +1,14 @@
 package ro.bankar
 
 import freemarker.cache.ClassTemplateLoader
-import io.ktor.server.application.Application
-import io.ktor.server.application.install
-import io.ktor.server.freemarker.FreeMarker
-import io.ktor.server.plugins.autohead.AutoHeadResponse
-import io.ktor.server.plugins.hsts.HSTS
+import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.plugins.hsts.*
 import org.jetbrains.exposed.sql.Database
 import ro.bankar.api.SmsService
 import ro.bankar.database.loadStaticData
-import ro.bankar.plugins.configureAuthentication
-import ro.bankar.plugins.configureRouting
-import ro.bankar.plugins.configureSerialization
-import ro.bankar.plugins.configureSessions
-import ro.bankar.plugins.configureSockets
-import ro.bankar.plugins.init
-import ro.bankar.plugins.reset
+import ro.bankar.plugins.*
 
 fun main(args: Array<String>) {
     if (args.size == 1 && args[0] == "reset") Database.reset()
@@ -31,7 +24,7 @@ fun Application.module() {
     SmsService.configure(
         System.getenv("SENDSMS_USER"),
         System.getenv("SENDSMS_KEY"),
-        environment.config.propertyOrNull("ktor.sms.reportURL")?.getString(),
+        System.getenv("SENDSMS_URL"),
     )
     loadStaticData()
     configureSerialization()
